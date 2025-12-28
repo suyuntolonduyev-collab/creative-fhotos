f27c26a0d0707869ddd971eb380dede7838214ab
suyuntolonduyev-collab <suyuntolonduyev@gmail.com>
2025-12-29 05:31:25 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "path"; 
+import { resolve } from "path";
 
 export default defineConfig({
-  base: "/creative-fhotos/",
+  base: "/creative-fhotos/", 
   plugins: [tailwindcss()],
   build: {
     outDir: "docs",
+    emptyOutDir: true, 
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
