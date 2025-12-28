018442e6c61e885d679db44440806dc015f99d9a
*Tolonduiev <potato.power.of.community@gmail.com>
2025-12-29 05:10:57 +0600

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/creative-fhotos/",
   plugins: [tailwindcss()],
   build: {
     outDir: "docs",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        exemples: resolve(__dirname, "exemples.html"),
+        vacansii: resolve(__dirname, "vacansii.html"),
+      },
+    },
   },
 });
