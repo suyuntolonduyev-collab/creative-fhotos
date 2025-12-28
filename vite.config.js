f8f7db269dfbdea38e6d7e696bc4373859580deb
*Tolonduiev <potato.power.of.community@gmail.com>
2025-12-29 04:42:47 +0600

    Сборка проекта в папку docs для GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "creative-fhotos",
+  base: "/creative-fhotos/",
   plugins: [tailwindcss()],
+  build: {
+    outDir: "docs",
+  },
 });
